90b38c6cfa5e7750ef79211eb7a95f668bbbcd1d
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2021-11-11 15:55:50 +0100

    README++

---
+++ README.md
@@ -0,0 +1,9 @@
+# Klipp
+
+Retrieval and local storage of notes and snippets to and from the copy buffer
+
+## Ideas for this project
+
+- Create a CLI for interacting with the application
+- Add optional retention to notes so that notes automatically can be deleted after a given amount of time, e.g. for tokens or similar
+- Encryption of some of the notes?
--- klipp.go
+++ klipp.go
@@ -1,38 +1,41 @@
 package main
 
 import (
-    "os"
-    "io/ioutil"
-    "log"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
+func makeTempFile(name string) *os.File {
+	tmpfile, err := ioutil.TempFile("", name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return tmpfile
+}
+
 func main() {
-    files, err := ioutil.ReadDir("./")
-    if err != nil {
-        log.Fatal(err)
-    }
-    for _, file := range files {
-        log.Println(file.Name(), "is directory?", file.IsDir())
-    }
-    
-    tmpfile, err := ioutil.TempFile("", "tempfile")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer os.Remove(tmpfile.Name()) // clean up
-    
-    message := []byte("\n\n\tHello World\n\n")
-    if _, err := tmpfile.Write(message); err != nil {
-        log.Fatal(err)
-    }
-    if err := tmpfile.Close(); err != nil {
-        log.Fatal(err)
-    }
-    
-    content, err := ioutil.ReadFile(tmpfile.Name())
-    log.Printf("File contents: %s", content)
-    if err != nil {
-        log.Fatal(err)
-        }    
-    }
-    
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		log.Println(file.Name(), "is directory?", file.IsDir())
+	}
+	tmpfile := makeTempFile("hush")
+	defer os.Remove(tmpfile.Name()) // clean up
+
+	message := []byte("\n\n\tHello World\n\n")
+	if _, err := tmpfile.Write(message); err != nil {
+		log.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	content, err := ioutil.ReadFile(tmpfile.Name())
+	log.Printf("File contents: %s", content)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
